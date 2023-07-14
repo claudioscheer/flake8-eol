efd741ba636667f8d9f824fa9fc8517e3197b529
Claudio Scheer <claudioscheer@protonmail.com>
2023-07-14 11:12:05 -0300

    feat: add setup.cfg and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="flake8-eol",
+    version="0.0.1",
+    description="Flake8 plugin to enforce EOL consistency",
+    url="https://github.com/claudioscheer/flake8-eol",
+    author="Claudio Scheer",
+    author_email="claudioscheer@protonmail.com",
+    install_requires=["flake8"],
+)
